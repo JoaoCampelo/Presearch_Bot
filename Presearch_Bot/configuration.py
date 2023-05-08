6908649754e552225c22e2f9578e505ece98ee24
João Campelo <26392@ufp.edu.pt>
2023-05-08 21:14:41 +0100

    Update configuration.py
    Add proxy config...

--- Presearch_Bot/configuration.py
+++ Presearch_Bot/configuration.py
@@ -1,8 +1,7 @@
 config = {
-    "delay": 4,  # Delay between searches (seconds)
+    "delay": 5,  # Delay between searches (seconds)
     "random": True,  # Loop through the search.txt words randomly
-    "searches_count": 50,  # Only if random is True
+    "searches_count": 60,  # Only if random is True
     "profile_number": 1,  # The Chrome's profile number starting from 1
+    "proxy": False #Use or not proxies
 }
-
-
